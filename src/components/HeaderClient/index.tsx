17290973230755a0e799b00ee2dfd4908a109a4c
Luis Fernando <luisfernandomgrs@gmail.com>
2025-03-11 00:09:52 -0300

    Code-review: Using by first time, a component react

---
+++ src/components/HeaderClient/index.tsx
@@ -0,0 +1,21 @@
+import cartIcon from '../../assets/cart.svg'
+
+export default function HeaderClient() {
+    return (
+        <header className="ds-header-client">
+            <nav className="dsc-container dsc-nav-client">
+                <h1>Big Store</h1>
+                <div className="nav-bar-right">
+                    <div className="dsc-menu-items-container">
+                        <div className="dsc-menu-item">
+                            <img src={cartIcon} alt="Checkout" />
+                        </div>
+                    </div>
+                    <div className="dsc-logged-user">
+                        <a href="#">Login</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
