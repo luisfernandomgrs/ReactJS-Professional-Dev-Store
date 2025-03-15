ef47f242c8b4cf5c08fdafc6ec666ee27c5e92f2
Luis Fernando <luisfernandomgrs@gmail.com>
2025-03-15 00:10:46 -0300

    Code-review: some more components...

---
+++ src/components/ProductCategory/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export default function ProductCategory() {
+    return (
+        /*
+        <>
+            <div className="dsc-category">Eletrônicos</div>
+            <div className="dsc-category">Computadores</div>
+        </>
+        */
+        <div className="dsc-category">Eletrônicos</div>
+    );
+}
