30d75422d69f0e2aadf5120465c0b86dcb49397e
Luis Fernando <luisfernandomgrs@gmail.com>
2025-03-14 22:18:30 -0300

    Code-review: First steps with components, HeaderComponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+
+import computerImg from "./assets/computer.png";
+import HeaderClient from "./components/HeaderClient";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>DSCommerce</h1>
-  );
+    return (
+        <>
+            <HeaderClient/> 
+            <main>
+                <section id="product-details-section" className="dsc-container">
+                    <div className="dsc-card dsc-mb20">
+                        <div className="dsc-product-details-top dsc-line-bottom">
+                            <img src={computerImg} alt="" />
+                        </div>
+                        <div className="dsc-product-details-bottom">
+                            <h3>R$ 5000,00</h3>
+                            <h4>Computador Gamer XT</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint
+                                occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <div className="dsc-category-container">
+                                <div className="dsc-category">Eletrônicos</div>
+                                <div className="dsc-category">Computadores</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dsc-btn-page-container">
+                        <div className="dsc-btn dsc-btn-blue">Buy</div>
+                        <div className="dsc-btn dsc-btn-white">Go Home</div>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
